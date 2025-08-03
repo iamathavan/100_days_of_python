7a06b72f88dc96121e16842b1b41abeafa00c2df
ATHAVAN <athavan9720king@gmail.com>
2025-08-03 15:30:21 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to band name creator!")
+city_name=input("Which city you grow up ?\n")
+pet_name=input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name )
